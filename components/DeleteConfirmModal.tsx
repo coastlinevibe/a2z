0e3220c77ba0d4d2217a1a20a4084856566291e4
Riegal <riegal9@outlook.com>
2025-10-18 10:56:51 +0200

    Fix ESLint error: escape quotes in DeleteConfirmModal

--- components/DeleteConfirmModal.tsx
+++ components/DeleteConfirmModal.tsx
@@ -63,7 +63,7 @@ export function DeleteConfirmModal({
             </h3>
             {itemName && (
               <p className="text-sm font-medium text-gray-700 mb-2">
-                "{itemName}"
+                &quot;{itemName}&quot;
               </p>
             )}
             <p className="text-gray-600">
